546e76f67d7899cd726169bd76d71d42f752226e
Andy Yong <kwokhou@gmail.com>
2013-11-20 23:45:30 +0800

    Added Creation unit tests for XDynamicWorkflow
    - Updated naming for workflow object
    - Added sample unit tests

--- src/XDynamicWorkflow/XDynamicWorkflow.cs
+++ src/XDynamicWorkflow/XDynamicWorkflow.cs
@@ -1,26 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace XDynamicWorkflow
 {
     public class XDynamicWorkflow
     {
-        readonly Dictionary<XStatePosition, string> _definitions;
+        readonly Dictionary<XStatePosition, string> _workflowDefinitions;
         public string CurrentState { get; set; }
 
-        public XDynamicWorkflow(Dictionary<XStatePosition, string> stateDefinitions, string initialState = null)
+        public XDynamicWorkflow(Dictionary<XStatePosition, string> workflowDefinitionses, string initialState)
         {
+            if (workflowDefinitionses == null || !workflowDefinitionses.Any())
+                throw new ArgumentNullException("workflowDefinitionses", "Missing Workflow definitions in constructor");
+
+            if (string.IsNullOrEmpty(initialState))
+                throw new ArgumentNullException("initialState", "Missing initial workflow state");
+
             CurrentState = initialState;
-            _definitions = stateDefinitions;
+            _workflowDefinitions = workflowDefinitionses;
         }
 
         private void FindNext(string actor, string action, Action<string> successAction, Action failAction)
         {
             var position = new XStatePosition(this.CurrentState, actor, action);
             string nextState;
-            if (!_definitions.TryGetValue(position, out nextState))
+            if (!_workflowDefinitions.TryGetValue(position, out nextState))
             {
                 if (failAction != null)
                     failAction();
---
+++ tests/XDynamicWorkflow.Tests/XDynamicWorkflowTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using XDynamicWorkflow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XDynamicWorkflow.Tests
+{
+    [TestClass]
+    public class XDynamicWorkflowTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Cannot_create_workflow_without_definitions()
+        {
+            var workflow = new XDynamicWorkflow(null, "UNKNOWN");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Cannot_create_workflow_with_empty_definitions()
+        {
+            var emptyDefinition = new Dictionary<XStatePosition, string>();
+            var workflow = new XDynamicWorkflow(emptyDefinition, "UNKNOWN");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Cannot_create_workflow_without_initialState()
+        {
+            var sampleDefinition = new Dictionary<XStatePosition, string>
+            {
+                {new XStatePosition("UNKNOWN", "REQUESTER", "SAVE AS DRAFT"), "DRAFT"},
+                {new XStatePosition("UNKNOWN", "REQUESTER", "SUBMIT FOR APPROVAL"), "PENDING"},
+                {new XStatePosition("DRAFT", "REQUESTER", "SUBMIT FOR APPROVAL"), "PENDING"}
+            };
+
+            var workflow = new XDynamicWorkflow(sampleDefinition, null);
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Can_create_workflow_with_initialize_state()
+        {
+            var sampleDefinition = new Dictionary<XStatePosition, string>
+            {
+                {new XStatePosition("UNKNOWN", "REQUESTER", "SAVE AS DRAFT"), "DRAFT"},
+                {new XStatePosition("UNKNOWN", "REQUESTER", "SUBMIT FOR APPROVAL"), "PENDING"},
+                {new XStatePosition("DRAFT", "REQUESTER", "SUBMIT FOR APPROVAL"), "PENDING"}
+            };
+
+            var workflow = new XDynamicWorkflow(sampleDefinition, "DRAFT");
+            Assert.AreEqual("DRAFT", workflow.CurrentState);
+        }
+    }
+}
