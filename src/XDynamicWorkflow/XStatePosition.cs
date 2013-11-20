3c091f1e3f79728e76053a676f51dc71f76819a3
Andy Yong <kwokhou@gmail.com>
2013-11-20 22:25:32 +0800

    Refactor XStatePosition & MoveNext

---
+++ src/XDynamicWorkflow/XStatePosition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XDynamicWorkflow
+{
+    public class XStatePosition
+    {
+        readonly string _state;
+        readonly string _action;
+        readonly string _actor;
+
+        public XStatePosition(string currentState, string actor, string action)
+        {
+            _state = currentState;
+            _actor = actor;
+            _action = action;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + _state.GetHashCode();
+                hash = hash * 31 + _action.GetHashCode();
+                hash = hash * 31 + _actor.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as XStatePosition;
+            return other != null && _state == other._state && _action == other._action && _actor == other._actor;
+        }
+    }
+}
---
+++ src/XDynamicWorkflow/XWorkflowProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XDynamicWorkflow
+{
+    public class XDynamicWorkflow
+    {
+        readonly Dictionary<XStatePosition, string> _definitions;
+        public string CurrentState { get; set; }
+
+        public XDynamicWorkflow(Dictionary<XStatePosition, string> stateDefinitions, string initialState = null)
+        {
+            CurrentState = initialState;
+            _definitions = stateDefinitions;
+        }
+
+        private void FindNext(string actor, string action, Action<string> successAction, Action failAction)
+        {
+            var position = new XStatePosition(this.CurrentState, actor, action);
+            string nextState;
+            if (!_definitions.TryGetValue(position, out nextState))
+            {
+                if (failAction != null)
+                    failAction();
+            }
+            else
+            {
+                CurrentState = nextState;
+                if (successAction != null)
+                    successAction(nextState);
+            }
+        }
+
+        public string MoveNext(string actor, string action, Action<string> successAction, Action failAction)
+        {
+            FindNext(actor, action, successAction, failAction);
+            return CurrentState;
+        }
+    }
+}
